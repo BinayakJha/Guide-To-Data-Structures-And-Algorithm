5645ee629a6b2b811b4194313d4069695e9f9fb0
BinayakJha <jha36binayak@gmail.com>
2023-04-25 09:55:49 +0545

    added new interview question

--- InterviewQuestions/AgeSort/main.py
+++ InterviewQuestions/AgeSort/main.py
@@ -1,6 +1,11 @@
 def sort_by_age(age_list):
-    sorted_list = []
-    
+    for i in range(len(age_list)):
+        for j in range(0,len(age_list)-i-1):
+            if age_list[j] > age_list[j+1]:
+                temp = age_list[j]
+                age_list[j] = age_list[j+1]
+                age_list[j+1] = temp
+    return age_list
 
 age_list = [33, 25, 99, 33, 50, 42]
 print(sort_by_age(age_list))
